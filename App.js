22f560d0c97ef7577f2fbd56fcf3666428e5760b
Leo <volwahsenl@gmail.com>
2023-10-28 17:11:01 +0200

    My project is just a prove of concept how to create a mobile app and change what the user see on the virtual android phone

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <h1>Hello User!</h1>
+      <Text style={styles.heading}>Hello User!</Text>
+      <Text >Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#38454C',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 54,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: '#fff',
   },
 });
---
+++ reademe.md
@@ -0,0 +1,4 @@
+## Terminal commands to use:
+**npx create-expo-app my-app**
+**npx expo**
+**npm run android**
